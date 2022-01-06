388b93004f51fcd066bf3ea859c700b9f5aa585b
loicnico96 <loicnico96@gmail.com>
2022-01-06 09:32:43 +0200

    Fetch account information

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,29 +1,53 @@
+import algosdk from "algosdk"
 import { PageLayout } from "components/Layout/PageLayout"
 import { Link } from "components/Primitives/Link"
 import { useNetworkContext } from "context/NetworkContext"
 import { useEffect, useState } from "react"
 
 export default function HomePage() {
-  const { api } = useNetworkContext()
+  const { indexer } = useNetworkContext()
 
+  const [address, setAddress] = useState("")
   const [data, setData] = useState<unknown>()
+  const [error, setError] = useState<Error>()
 
   useEffect(() => {
-    api
-      .getTransactionParams()
-      .do()
-      .then(params => {
-        setData(params)
-      })
-      .catch(error => {
-        setData(error)
-      })
-  }, [api])
+    if (algosdk.isValidAddress(address)) {
+      setData(undefined)
+      setError(undefined)
+      indexer
+        .lookupAccountByID(address)
+        .do()
+        .then(params => {
+          setData(params)
+          setError(undefined)
+        })
+        .catch(error => {
+          setError(error)
+        })
+    } else {
+      setError(Error("Invalid address"))
+    }
+  }, [address, indexer])
 
   return (
     <PageLayout>
       <div>You are on Mainnet.</div>
-      <div>{data ? JSON.stringify(data, undefined, 2) : "Loading..."}</div>
+      <div>
+        Address:
+        <input
+          type="text"
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        {error
+          ? error.message
+          : data
+          ? JSON.stringify(data, undefined, 2)
+          : "Loading..."}
+      </div>
       <div>
         <Link href="https://github.com/loicnico96/algorand-tools">
           View on GitHub
